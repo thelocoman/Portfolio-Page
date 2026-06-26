85b6860f79e7f1ed26d078b5d7bed63c8012a237
Tibor Lovasz <tiborlovasz@icloud.com>
2026-06-26 13:18:34 +0200

    Changed the yml, because the autodeploy didn't triger

--- Client/portfolio-page/next.config.ts
+++ Client/portfolio-page/next.config.ts
@@ -2,6 +2,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',          // <-- Enforces the creation of the 'out/' folder
+  images: {
+    unoptimized: true,       // <-- Prevents build failures if using <Image /> components
+  },
   /* This completely silences the HMR warning on dev server ports */
   logging: {
     fetches: {
