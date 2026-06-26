9040030f88359d3dba985ca2ac092d30cf449eae
Tibor Lovasz <tiborlovasz@icloud.com>
2026-06-26 12:42:58 +0200

    Final .yml file

--- Client/portfolio-page/app/contact.tsx
+++ Client/portfolio-page/app/contact.tsx
@@ -1,4 +1,4 @@
-import { Html5Original, GithubOriginal, LinkedinPlain } from 'devicons-react';
+import { GithubOriginal, LinkedinPlain } from 'devicons-react';
 
 export default function Contact() {
   return (
